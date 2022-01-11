bc7b0daac86ab2fc9bf492e9942831df6fe4bc26
snapkov <snapkov@gmail.com>
2022-01-12 00:41:03 +0300

    feat: impliment function check correct sequence of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
+  let isFind = true;
+  while ((str.length > 0) && (isFind === true)) {
+    let subBrackets = "";
+    let indexBrackets = 0;
+    isFind = false;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      subBrackets = bracketsConfig[i].join("");
+      if (str.indexOf(subBrackets) > -1) {
+        isFind = true;
+        indexBrackets = str.indexOf(subBrackets);
+        str = str.substr(0, indexBrackets) + str.substr(indexBrackets + 2, str.length);
+      }
+    }
+  }
+  return (str.length === 0) ? true : false;
   // your solution
 }
